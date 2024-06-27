c99c3343d484c8be8afbf81a35941020b99944d0
Keerthanachinthagattu35 <keerthanachinthagattu@gmail.com>
2024-06-27 16:25:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String[] args) {
+        int a,b,c;
+        a=50;             
+        b=23;             
+        c=a+b;              
+        System.out.println(c); 
+    }
+}
