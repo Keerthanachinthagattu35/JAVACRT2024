279dfbc2c747455d6d51c65ac166ba3c8f3bc964
Keerthanachinthagattu35 <keerthanachinthagattu@gmail.com>
2024-06-27 16:22:04 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,16 @@
+class Function{
+    public static int fun(int n)
+    {
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c++;
+        }
+        return c;
+    }
+public static void main(String[] args)
+{
+    int n=100;
+    System.out.println("N=100, no of instructions 0(n) :"+fun(n));
+}
+}
