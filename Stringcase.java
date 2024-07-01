910c60df328875c8cb7ec184d69882eb4d53ae0d
Keerthanachinthagattu35 <keerthanachinthagattu@gmail.com>
2024-07-01 12:17:07 +0530

    Add files via upload

---
+++ Stringcase.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Stringcase
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String s1="Keerthana";
+          System.out.println(s1.toUpperCase());
+          System.out.println(s1.toLowerCase());
+        
+    }
+}
