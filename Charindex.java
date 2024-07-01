6ab9489b772da20fac5f3b0a6a97a383d1a05dbf
Keerthanachinthagattu35 <keerthanachinthagattu@gmail.com>
2024-07-01 10:31:32 +0530

    Add files via upload

---
+++ Charindex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Charindex {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=new String("Satish Software Solutions");
+        System.out.println(str);
+         System.out.println(str.replace('S','s'));
+          System.out.println(str.charAt(7));
+           System.out.println(str.indexOf("i"));
+            System.out.println(str.lastIndexOf("i"));
+        
+    }
+}
