413833f106c8a4db79719aca18b27042e13970ef
Keerthanachinthagattu35 <keerthanachinthagattu@gmail.com>
2024-07-02 09:48:49 +0530

    Add files via upload

---
+++ Substring2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Substring2 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str="Java is simple Language";
+        System.out.println(str.substring(6));
+        System.out.println(str.substring(3,15));
+      
+    }
+}
